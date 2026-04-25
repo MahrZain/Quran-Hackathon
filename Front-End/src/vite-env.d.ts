e23d9a5f88c4414a4922045951277df55b1fab7c
M.Zain <zainzainpubg1122@gmail.com>
2026-04-25 15:23:14 +0500

    refactor: move pwa-install type definition from standalone file to vite-env.d.ts

--- Front-End/src/vite-env.d.ts
+++ Front-End/src/vite-env.d.ts
@@ -8,3 +8,23 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+declare module '@khmyznikov/pwa-install';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'pwa-install': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+        name?: string;
+        description?: string;
+        icon?: string;
+        'manifest-url'?: string;
+        'manual-apple'?: string | boolean;
+        'manual-chrome'?: string | boolean;
+        'disable-chrome'?: string | boolean;
+        'install-description'?: string;
+        'use-local-storage'?: string | boolean;
+      }, HTMLElement>;
+    }
+  }
+}
